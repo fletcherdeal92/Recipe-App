8919735a8543103eec6d7840039cb93eb55a6619
Fletcher Deal <fletcherdeal@gmail.com>
2021-10-13 12:23:14 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Fletcher Deal on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
