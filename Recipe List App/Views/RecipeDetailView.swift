fb9a83e4c0d731a58799e007c0c9e60966a60bc5
Fletcher Deal <fletcherdeal@gmail.com>
2021-10-28 23:30:11 -0400

    - Add RecipeDetailView for featured recipes - Add button, action for RecipeDetailView

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -21,6 +21,13 @@ struct RecipeDetailView: View {
                     .resizable()
                     .scaledToFill()
                 
+                // MARK: Recipe Title
+                Text(recipe.name)
+                    .bold()
+                    .padding(.top, 20)
+                    .padding(.leading)
+                    .font(.largeTitle)
+                
                 // MARK: Serving Size Picker
                 VStack(alignment: .leading) {
                     Text("Select your serving size:")
@@ -63,7 +70,6 @@ struct RecipeDetailView: View {
                 .padding(.horizontal)
             }
         }
-        .navigationTitle(recipe.name)
     }
 }
 
